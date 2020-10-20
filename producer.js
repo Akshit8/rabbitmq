a81a4bb85093252b7b2f346e98f37ae9ee960e2f
akshit <akshitsadana@gmail.com>
2020-10-20 23:10:41 +0530

    producer impl1

---
+++ producer.js
@@ -0,0 +1,27 @@
+const amqp = require('amqplib/callback_api');
+const argv = require('minimist')(process.argv.slice(1));
+
+const { host } = argv;
+
+/**
+ * implemenation with callbacks
+ * basic implementation
+ * channel connection close not handled
+ */
+const impl1 = async () => {
+    amqp.connect(`amqp://${host}`, (err0, connection) => {
+        if(err0) throw err0;
+        connection.createChannel((err1, channel) => {
+            if(err1) throw err1;
+            const queue = 'hello';
+            const msg = `Hello World at ${Date()}`;
+
+            channel.assertQueue(queue, {
+                durable: false
+            });
+
+            channel.sendToQueue(queue, Buffer.from(msg));
+            console.log('message published');
+        });
+    });
+};
